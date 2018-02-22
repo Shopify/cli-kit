b52266f1feea0df38f84e34d223b2948a75a9427
Lisa Ugray <lisa.ugray@shopify.com>
2018-02-22 13:58:49 -0500

    Consolidate config

--- lib/cli/kit/resolver.rb
+++ lib/cli/kit/resolver.rb
@@ -3,17 +3,12 @@ require 'cli/kit'
 module CLI
   module Kit
     class Resolver
-      def initialize(command_registry:, error_handler:)
-        @command_registry = command_registry
-        @error_handler = error_handler
-      end
-
       def call(args)
         args = args.dup
         command_name = args.shift
 
-        @error_handler.handle_abort do
-          command, command_name = @command_registry.lookup_command(command_name)
+        CLI::Kit.error_handler.handle_abort do
+          command, command_name = CLI::Kit.command_registry.lookup_command(command_name)
           return [command, command_name, args]
         end
 
