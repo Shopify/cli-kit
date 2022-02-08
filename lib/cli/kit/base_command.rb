9d6f979007769ac43ee744b22dea99c60356e0f3
Burke Libbey <burke.libbey@shopify.com>
2022-02-08 16:58:36 -0500

    Remove a redundant rescue/raise

--- lib/cli/kit/base_command.rb
+++ lib/cli/kit/base_command.rb
@@ -17,12 +17,7 @@ module CLI
 
       sig { params(args: T::Array[String], command_name: String).void }
       def self.call(args, command_name)
-        cmd = new
-        begin
-          cmd.call(args, command_name)
-        rescue Exception => e # rubocop:disable Lint/RescueException
-          raise e
-        end
+        new.call(args, command_name)
       end
 
       sig { returns(T::Boolean) }
